6510b6b4d146cc5430d5f5de51a1caba229b26b4
codkpl <kolyabro56@gmail.com>
2021-03-29 20:41:04 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.053d00a122c3ba06ff928caf61785961.js"
+  "./precache-manifest.4d88f2c3101911079da44d8a98277185.js"
 );
 
 workbox.clientsClaim();
