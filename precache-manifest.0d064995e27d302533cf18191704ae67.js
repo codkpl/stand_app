b2fe097160643233a78c98903b9e0f4f1e8cf63a
codkpl <kolyabro56@gmail.com>
2021-03-29 19:41:46 +0500

    Updates

---
+++ precache-manifest.0d064995e27d302533cf18191704ae67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca43ee06bc8297656aa4",
+    "url": "./static/css/main.2cb52ab0.chunk.css"
+  },
+  {
+    "revision": "ca43ee06bc8297656aa4",
+    "url": "./static/js/main.63796724.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "9e9f6c8357c313970588",
+    "url": "./static/css/2.8c106557.chunk.css"
+  },
+  {
+    "revision": "9e9f6c8357c313970588",
+    "url": "./static/js/2.f8e4dd41.chunk.js"
+  },
+  {
+    "revision": "c1835a8b63322a33237b68f4f60eadca",
+    "url": "./index.html"
+  }
+];
